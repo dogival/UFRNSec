abf25e9619215757896ff0d474c4be9e2172ce43
Dogival Ferreira <dogivalfer@gmail.com>
2017-10-31 08:17:20 -0300

    Initial commit

---
+++ app/src/main/java/br/ufrn/security/ufrnsec/MenuInicial.java
@@ -0,0 +1,13 @@
+package br.ufrn.security.ufrnsec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_inicial);
+    }
+}
