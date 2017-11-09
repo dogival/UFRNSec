62a1b05d0b4d7dc5daf5e51e43ff47943e25192f
Rubens Araújo <rubensaviana@hotmail.com>
2017-11-08 23:53:09 -0300

    Criada Activity e Intent "Emergencia".

---
+++ app/src/main/java/br/ufrn/security/ufrnsec/EmergenciaActivity.java
@@ -0,0 +1,13 @@
+package br.ufrn.security.ufrnsec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmergenciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergencia);
+    }
+}
--- app/src/main/java/br/ufrn/security/ufrnsec/MenuInicial.java
+++ app/src/main/java/br/ufrn/security/ufrnsec/MenuInicial.java
@@ -2,6 +2,9 @@ package br.ufrn.security.ufrnsec;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.*;
+import android.widget.*;
+import android.view.*;
 
 public class MenuInicial extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MenuInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_inicial);
+
+        Button botaoEmergencia = (Button) findViewById(R.id.emergencia);
+
+        botaoEmergencia.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent it = new Intent(MenuInicial.this, EmergenciaActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
