152281b97e1f3ed822d48fe8ab6c72f4a40b3cdc
Dogival Ferreira <dogivalfer@gmail.com>
2017-12-07 09:01:46 -0300

    Adicionado localização
    Na activity principal a localização do usuário é adquirida assim que o botão de emergência é clicado.

--- app/src/main/java/br/ufrn/security/ufrnsec/MenuInicial.java
+++ app/src/main/java/br/ufrn/security/ufrnsec/MenuInicial.java
@@ -2,8 +2,14 @@ package br.ufrn.security.ufrnsec;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationListener;
 import android.net.Uri;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +22,8 @@ import android.view.*;
 public class MenuInicial extends AppCompatActivity {
 
     public static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 99;
+    LocationManager locationManager;
+    double longitude, latitude;
 
     public void funcContato(View view){
         Intent it = new Intent(MenuInicial.this, contatoActivity.class);
@@ -27,7 +35,7 @@ public class MenuInicial extends AppCompatActivity {
         startActivity(it);
     }
 
-    public void chamar(View view){
+    public void chamar(View view) {
         try {
             Intent callIntent = new Intent(Intent.ACTION_CALL);
             callIntent.setData(Uri.parse("tel:84988175658"));
@@ -43,31 +51,111 @@ public class MenuInicial extends AppCompatActivity {
                     }
                 });
             }
-            
-                ActivityCompat.requestPermissions(MenuInicial.this,
-                        new String[]{Manifest.permission.CALL_PHONE},
-                        MY_PERMISSIONS_REQUEST_CALL_PHONE);
-                startActivity(callIntent);
+
+            ActivityCompat.requestPermissions(MenuInicial.this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE);
+            startActivity(callIntent);
 
         } catch (ActivityNotFoundException activityException) {
             Log.e("Chamando a segurança", "Ligação falhou", activityException);
         }
-
     }
+
+    private boolean checkLocation() {
+        if(!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
+    }
+
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Habilitar Localização")
+                .setMessage("Suas configurações de localização estão desabilitadas.\nPor favor ative a localização para" +
+                        "usar este aplicativo")
+                .setPositiveButton("Configurações de Localização", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
+    private final LocationListener locationListenerBest = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MenuInicial.this, "Usuário localizado", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_inicial);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
         Button botaoEmergencia = (Button) findViewById(R.id.emergencia);
 
         botaoEmergencia.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
+
+                checkLocation();
+                Criteria criteria = new Criteria();
+                criteria.setAccuracy(Criteria.ACCURACY_FINE);
+                criteria.setAltitudeRequired(false);
+                criteria.setBearingRequired(false);
+                criteria.setCostAllowed(true);
+                criteria.setPowerRequirement(Criteria.POWER_LOW);
+                String provider = locationManager.getBestProvider(criteria, true);
+                if(provider != null) {
+                    try {
+                        locationManager.requestLocationUpdates(provider, 2 * 60 * 1000, 10, locationListenerBest);
+                    } catch (SecurityException se) {};
+                }
+
                 Intent it = new Intent(MenuInicial.this, EmergenciaActivity.class);
                 startActivity(it);
             }
         });
+
+
 
     }
 }
